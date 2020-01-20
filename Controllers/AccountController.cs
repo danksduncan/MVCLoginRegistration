71235f98a6b956e982dc2e6cb5156cf19514e257
danksduncan <danksduncan@gmail.com>
2020-01-19 22:16:39 -0500

    Added Register and Login to AccountController.

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MVCLoginRegistration.Models;
 
 namespace MVCLoginRegistration.Controllers
 {
@@ -11,6 +12,53 @@ namespace MVCLoginRegistration.Controllers
         // GET: Account
         public ActionResult Index()
         {
+            return View();
+        }
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Register(UserAccount account)
+        {
+            if (ModelState.IsValid)
+            {
+                using (UserDbContext db = new UserDbContext())
+                {
+                    db.userAccount.Add(account);
+                    db.SaveChanges();
+                }
+                ModelState.Clear();
+                ViewBag.Message = account.FirstName + " " + account.LastName + " successfully registered. ";
+            }
+            return View();
+        }
+
+        //Login
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(UserAccount user)
+        {
+            using (UserDbContext db = new UserDbContext())
+            {
+                var usr = db.userAccount.Single(u => u.Email == user.Email && u.Password == user.Password);
+                if (usr != null)
+                {
+                    Session["UserID"] = usr.UserID.ToString();
+                    Session["Email"] = usr.Email.ToString();
+                    return RedirectToAction("LoggedIn");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Username or Password is wrong.");
+                }
+            }
             return View();
         }
     }
