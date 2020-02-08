f37755bd3b21edf41081dc118c0fdc553ed6fc9a
danksduncan <danksduncan@gmail.com>
2020-02-07 19:24:23 -0500

    Updated UserAccount display names for all rows.

--- Models/UserAccount.cs
+++ Models/UserAccount.cs
@@ -12,19 +12,24 @@ namespace MVCLoginRegistration.Models
         public int UserID { get; set; }
 
         [Required(ErrorMessage = "First name is required.")]
+        [Display(Name = "First Name")]
         public string FirstName { get; set; }
 
         [Required(ErrorMessage = "Last name is required.")]
+        [Display(Name = "Last Name")]
         public string LastName { get; set; }
 
         [Required(ErrorMessage = "Email is required.")]
+        [Display(Name = "Email")]
         public string Email { get; set; }
 
         [Required(ErrorMessage = "Password is required.")]
+        [Display(Name = "Password")]
         [DataType(DataType.Password)]
         public string Password { get; set; }
 
         [Required(ErrorMessage = "Please conrfirm your password.")]
+        [Display(Name = "Confirm Password")]
         [DataType(DataType.Password)]
         public string ConfirmPassword { get; set; }
     }
